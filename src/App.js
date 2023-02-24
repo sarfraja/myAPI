06e84617d12152f47198766bbc4ebb1575c3fc53
Design Shore Mac mini <dsmacmini@Designs-Mac-mini.local>
2023-02-24 14:42:17 +0530

    files are added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ function App() {
     <div className="App">
      <a href="/"> Home </a> 
      <a href="/about"> About </a>
-     
+      <BrowserRouter>
+     <Routes>
+      <Route exact path="/" element={<About />} />
+      <Route exact path="/about" element={<GithubUsers title="Title" description="Discriptions" url="https://api.github.com/users" />} />
+      </Routes>
+      </BrowserRouter>
       
       
     </div>
